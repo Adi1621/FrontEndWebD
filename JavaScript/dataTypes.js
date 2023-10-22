f3d6ac1eeb3a7bab0754eadce4b3f2e13238eafd
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-22 23:51:56 +0530

    first commit

---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,41 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "aditya"
+let age = "18"
+let isLoggedIn = false
+
+// number => 2 to power 53
+// use bigint for larger number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object - JS is a object-based language. Everythning is a object in JS
+
+//typeof -find datatypes of a variable
+
+
+/**************** Datatype Conversion ******************/
+let score = "33" 
+console.log(typeof score) //string
+
+let scoreValue = Number(score) //converted into number
+console.log(typeof scoreValue)
+//"111" => 111
+//"111abc" => NaN
+//true => 1; false => 0
+
+/******************* Operations ***********************/
+let value = 14
+let updated_value = -value 
+console.log(updated_value)
+
+
+
+
+
+
+
+
---
+++ JavaScript/variables.js
@@ -0,0 +1,12 @@
+//declaring constant variable that cannot be reassigned another value
+const accountId = 1234
+
+//declaring variables that can be reassigned another value
+let accountEmail = "xyz.com"
+accountName = "Rohan"
+let accountCountry
+
+accountEmail = "abc.com"//re assigining the value 
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountName, accountCountry])
