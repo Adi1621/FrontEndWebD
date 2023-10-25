19480ce9d9f87141cf9319dffb70a14864ea39aa
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-25 13:10:45 +0530

    Memory Allocation

--- JavaScript/memoryAllocation.js
+++ JavaScript/memoryAllocation.js
@@ -1,8 +1,21 @@
 // Stack (Primitive), Heap (Non-Primitive)
 
+// stack use copy of data
 let myName = "Aditya Kanswal"
 let anotherName = myName
 anotherName = "Mohan"
 
 console.log(myName)
 console.log(anotherName)
+
+
+// heap use reference
+let userOne = {
+    email: "user@xyz.com"
+}
+
+let userTwo = userOne
+userTwo.email = "user@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
