c1405b8a3e350a3144774327c38b8302718b2990
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-25 23:53:29 +0530

    Number datatype in js

---
+++ JavaScript/numsAndMath.js
@@ -0,0 +1,14 @@
+const number = new Number(100)
+//functions in Number
+console.log(number.toString())
+console.log(number.toFixed(2))
+
+const number2 = 123.8444
+console.log(number2.toPrecision(3)) // return the value in String 
+
+const number3 = 1000000000
+console.log(number3.toLocaleString('en-In'))
+
+/**********************************Math******************************/
+
+console.log()
