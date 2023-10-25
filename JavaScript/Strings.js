8cfb901d725a8975186204fb241258b0c6f54fed
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-25 13:43:53 +0530

    String

---
+++ JavaScript/Strings.js
@@ -0,0 +1,20 @@
+const name = "Aditya Kanswal"
+const age = 18
+
+//String concatenation
+console.log(name + age)
+
+//String interpolation
+console.log(`Hello my name is ${name} and my age is ${age}`)
+
+const name2 = new String('Hail India')
+//some functions in string
+console.log(name2[0])
+console.log(name2.length)
+console.log(name2.toUpperCase())
+console.log(name2.substring(5, 11))
+console.log(name2.trim())
+console.log(name2.includes('Great'))
+
+//convert to array
+console.log(name2.split(" "))
